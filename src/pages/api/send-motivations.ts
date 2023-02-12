e288be1d6330a2a12975a55bce8b46f3cab9117e
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-02-12 11:24:29 +0700

    Send motivations api route

---
+++ src/pages/api/send-motivations.ts
@@ -0,0 +1,49 @@
+import nodemailer from 'nodemailer';
+import axios from 'axios';
+import { prisma } from '../../server/db';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Quote = {
+    q: string;
+    a: string;
+    i: string;
+    c: string;
+    h: string;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === 'POST') {
+        const transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.MOTIVATIONAL_QUOTES_EMAIL,
+                pass: process.env.MOTIVATIONAL_QUOTES_EMAIL_PASSWORD,
+            },
+        });
+
+        const randomQuoteUrl = 'https://zenquotes.io/api/random';
+        const subscribers = await prisma.subscriber.findMany();
+
+        await Promise.all(subscribers.map(async subscriber => {
+            const response = await axios.get(randomQuoteUrl);
+            const [quote] = response.data as Quote[];
+
+            if (!quote) {
+                return;
+            }
+
+            const text = `${quote.q}\n-${quote.a}`;
+
+            await transporter.sendMail({
+                from: '"Motivational Quotes" <motivationalquotes2102@gmail.com>',
+                to: subscriber.email,
+                subject: 'Your Motivation for Today',
+                text,
+            });
+        }));
+
+        res.status(200).send({ message: 'Motivatonal emails sent' });
+    }
+}
